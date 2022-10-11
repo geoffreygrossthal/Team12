63e0c368c9042fa66e0ba4bbb29815041eddead2
geoffreygrossthal <geoffreygrossthal@gmail.com>
2022-10-11 09:43:32 -0700

    Update

---
+++ App1.java
@@ -0,0 +1,32 @@
+
+
+
+import java.io.IOException;
+
+import javafx.application.Application;
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.layout.StackPane;
+import javafx.stage.Stage;
+ 
+public class App1 extends Application {
+    @Override
+    public void start(Stage primaryStage) {
+        
+  
+  Parent root;
+  try {
+    root = FXMLLoader.load(getClass().getResource("MainScene.fxml"));
+} catch (IOException e) {
+
+}
+    }
+ 
+ public static void main(String[] args) {
+        launch(args);
+    }
+}
--- src/hello.java
+++
@@ -1,9 +0,0 @@
-package src;
-public class hello {
-
-    public static void main(String[] args){
-
-        System.out.println("Github repository push test");
-    }
-    
-}
