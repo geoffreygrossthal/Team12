3dfa708aee5815b22cefc7e41cda87122fb5a580
geoffreygrossthal <geoffreygrossthal@gmail.com>
2022-11-09 01:58:05 -0700

    Update

--- SceneController.java
+++ SceneController.java
@@ -2,10 +2,12 @@
 import java.io.IOException;
 
 import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Node;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.control.TextField;
 import javafx.stage.Stage;
 
 public class SceneController {
@@ -13,6 +15,13 @@ public class SceneController {
     private Stage stage;
     private Scene scene;
     private Parent root;
+
+    @FXML
+    private TextField employeeIDChef;
+    /*@FXML
+    private TextField employeeIDProcesor;*/
+    @FXML
+    private TextField asuriteID;
  
     public void switchToSelectPizzaScene(ActionEvent event) throws IOException {
         root = FXMLLoader.load(getClass().getResource("SelectPizzaScene.fxml"));
@@ -24,8 +33,8 @@ public class SceneController {
         stage.show();
     }
  
-    public void switchToEmployeeLoginScene(ActionEvent event) throws IOException {
-        Parent root = FXMLLoader.load(getClass().getResource("EmployeeLoginScene.fxml"));
+    public void switchToEmployeeLoginProcessorScene(ActionEvent event) throws IOException {
+        Parent root = FXMLLoader.load(getClass().getResource("EmployeeLoginProcessorScene.fxml"));
         stage = (Stage)((Node)event.getSource()).getScene().getWindow();
         scene = new Scene(root);
         /*String css = this.getClass().getResource("styles.css").toExternalForm();
@@ -34,8 +43,21 @@ public class SceneController {
         stage.show();
     }
 
-    public void switchToEmployeeChefLoginScene(ActionEvent event) throws IOException {
+    public void switchToEmployeeLoginChefScene(ActionEvent event) throws IOException {
         Parent root = FXMLLoader.load(getClass().getResource("EmployeeLoginChefScene.fxml"));
+        stage = (Stage)((Node)event.getSource()).getScene().getWindow();
+        scene = new Scene(root);
+        /*String css = this.getClass().getResource("styles.css").toExternalForm();
+        scene.getStylesheets().add(css);*/
+        stage.setScene(scene);
+        stage.show();
+    }
+
+    @FXML
+    public void switchToChefScene(ActionEvent event) throws IOException {
+        Stage mainWindow = (Stage) employeeIDChef.getScene().getWindow();
+        String passwordChef = employeeIDChef.getText();
+        Parent root = FXMLLoader.load(getClass().getResource("ChefScene.fxml"));
         stage = (Stage)((Node)event.getSource()).getScene().getWindow();
         scene = new Scene(root);
         /*String css = this.getClass().getResource("styles.css").toExternalForm();
@@ -54,7 +76,10 @@ public class SceneController {
         stage.show();
     }
 
+    @FXML
     public void switchToOrderStatusScene(ActionEvent event) throws IOException {
+        Stage mainWindow = (Stage) asuriteID.getScene().getWindow();
+        String passwordStudent = asuriteID.getText();
         Parent root = FXMLLoader.load(getClass().getResource("OrderStatusScene.fxml"));
         stage = (Stage)((Node)event.getSource()).getScene().getWindow();
         scene = new Scene(root);
