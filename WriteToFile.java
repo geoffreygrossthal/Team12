7821db6977e21bf745b33118220e3c08a04d88a3
FlavaDave92 <117933026+FlavaDave92@users.noreply.github.com>
2022-11-11 17:40:54 -0700

    Add files via upload
    debugged version

--- WriteToFile.java
+++ WriteToFile.java
@@ -1,46 +1,41 @@
 
-
 import java.io.*;
 import java.util.Scanner;
 
 class WriteToFile
 {
 	// File manipulate class
-	private Scanner infile;
-	private File file;
-	private FileWriter ofile;
+	//private Scanner infile;
+	//private File file;
+	//private FileWriter ofile;
 
 	// array of String
-	private String orders;
+	//private String orders;
 
 	public static void addNewOrder(String order)
 	{
 		// read all data in file, then add new order
 		try {
-			file = new File("PizzaOrders.txt");
-			infile = new Scanner(file);
-
-			orders = order;
+			File file = new File("PizzaOrders.txt");
+			Scanner infile = new Scanner(file);
+			String orders = "";
 			String temp;
 			while (infile.hasNextLine())
 			{
-				orders = orders + "\n";
+				
 				temp = infile.nextLine();
 				orders = orders + temp;
+				orders = orders + "\n";
 			}
-
-		} catch (FileNotFoundException e) {
-			e.printStackTrace();
-		}
-
-		// after adding new order, write it back to file
-		try {
-			ofile = new FileWriter("PizzaOrders.txt");
+			orders = orders+order;
+			infile.close();
+			FileWriter ofile = new FileWriter("PizzaOrders.txt");
 			ofile.write(orders);
 			ofile.close();
-			
-		} catch (IOException e){
+
+		} catch (IOException e) {
 			e.printStackTrace();
 		}
+
 	}
 }
