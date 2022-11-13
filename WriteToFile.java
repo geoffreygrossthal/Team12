f5878c94996a13a8efaa7fc214ae21f2eead5cf7
ledacvien <53806253+ledacvien@users.noreply.github.com>
2022-11-13 15:59:43 -0700

    Update WriteToFile.java

--- WriteToFile.java
+++ WriteToFile.java
@@ -1,4 +1,3 @@
-
 import java.io.*;
 import java.util.Scanner;
 
@@ -8,11 +7,11 @@ class WriteToFile
 	{
 		// read all data in file, then add new order at the end of the file
 		try {
-			File file = new File("PizzaOrders.txt");
+			File file = new File("C:/Users/levie/Documents/Java_file/ASU_Pizza_Order/src/PizzaOrders.txt");
 			Scanner infile = new Scanner(file);
 			String orders = "";
 			String temp;
-			while (infile.hasNextLine())
+			while (infile.hasNext())
 			{
 				
 				temp = infile.nextLine();
@@ -21,7 +20,54 @@ class WriteToFile
 			}
 			orders = orders+order;
 			infile.close();
-			FileWriter ofile = new FileWriter("PizzaOrders.txt");
+			FileWriter ofile = new FileWriter("C:/Users/levie/Documents/Java_file/ASU_Pizza_Order/src/PizzaOrders.txt");
+			ofile.write(orders);
+			ofile.close();
+
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+
+	}
+
+	public static String orderIDgen()
+	{
+		int newID = 0;
+        try {
+			File file = new File("C:/Users/levie/Documents/Java_file/ASU_Pizza_Order/src/OrdersID.txt");
+			try (Scanner infile = new Scanner(file)) {
+				while (infile.hasNext())
+				{
+					newID = infile.nextInt();
+				}
+				infile.close();
+			}
+
+		} catch (FileNotFoundException e) {
+			e.printStackTrace();
+		}
+
+		newID++;
+        return String.format("%d", newID);
+	}
+
+	public static void writeOrderID (String id)
+	{
+		// read all data in file, then add new order at the end of the file
+		try {
+			File file = new File("C:/Users/levie/Documents/Java_file/ASU_Pizza_Order/src/OrdersID.txt");
+			Scanner infile = new Scanner(file);
+			String orders = "";
+			String temp;
+			while (infile.hasNext())
+			{
+				temp = infile.next();
+				orders = orders + temp;
+				orders = orders + "\n";
+			}
+			orders = orders + id;
+			infile.close();
+			FileWriter ofile = new FileWriter("C:/Users/levie/Documents/Java_file/ASU_Pizza_Order/src/OrdersID.txt");
 			ofile.write(orders);
 			ofile.close();
 
