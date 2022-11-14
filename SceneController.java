aae274c2cad1ff25bedd4baecab7a0ee1cfb5af4
geoffreygrossthal <geoffreygrossthal@gmail.com>
2022-11-13 17:38:14 -0700

    Update

--- SceneController.java
+++ SceneController.java
@@ -8,7 +8,8 @@ import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
-import javafx.scene.Node;import java.io.IOException;
+import javafx.scene.Node;
+import java.io.IOException;
 import java.net.URL;
 import java.util.ResourceBundle;
 
@@ -117,8 +118,8 @@ public class SceneController {
 
     @FXML
     private ChoiceBox<String> orderIDready;
-    @FXML
-    private ChoiceBox<String> orderIDnotReady;
+    //@FXML
+    //private ChoiceBox<String> orderIDnotReady;
     @FXML
     public void refreshProcessor(ActionEvent event) throws IOException
     {
@@ -314,6 +315,8 @@ public class SceneController {
         bar.setProgress(barFloat);
     }
 }
+
+/* 
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
@@ -574,4 +577,4 @@ public class SceneController {
     {
         bar.setProgress(barFloat);
     }
-}
+}*/
